d3f77d86ed9df640184a18a4e9fddac9071ada7b
juanoliver995 <95033445+juanoliver995@users.noreply.github.com>
2022-02-23 18:01:29 -0300

    Add files via upload

---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="nav">
+                <a src="">NOMBRE DE LA TIENDA</a>
+                <div className="nav__links">
+                    <a href="">Productos</a>
+                    <a href="">Contacto</a>
+                    <a href="">Quienes Somos</a>
+                </div>
+            </nav>
+        </header >
+    )
+}
+export default Navbar
